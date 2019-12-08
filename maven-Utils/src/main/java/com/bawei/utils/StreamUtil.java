7228bc71e98443dfe7d3541682364b850ce5ed58
asus <794119141@qq.com>
2019-12-08 19:00:49 +0800

    完善工具类

--- maven-Utils/src/main/java/com/bawei/utils/StreamUtil.java
+++ maven-Utils/src/main/java/com/bawei/utils/StreamUtil.java
@@ -1,56 +1,102 @@
 package com.bawei.utils;
 
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+/**
+ * 流工具类
+ * @Description:TODO(描述这个类的作用)   
+ * @author: zhanggm1002
+ * @date:   2019年12月5日 下午1:31:39
+ */
 public class StreamUtil {
-
 	/**
-	 * 
-	* @Title: closeAll 
-	* @Description: 关闭流
-	* @param @param autoCloseables    设定文件 
-	* @return void    返回类型 
-	* @throws
+	 * 关闭流的方法
+	 * @Title: closeAll   
+	 * @Description: 数组参数，可以批量删除多个打开的流   
+	 * @param: @param autoCloseables      
+	 * @return: void      
+	 * @throws
 	 */
-	public static void closeAll(AutoCloseable... autoCloseables) {
+	public static void closeAll(AutoCloseable... autoCloseables ) {
 		if(autoCloseables!=null) {
-			for (AutoCloseable autoCloseable : autoCloseables) {
+			for(AutoCloseable autoCloseable:autoCloseables) {
 				try {
 					autoCloseable.close();
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 	
-	public static String getFileContent(File file) {
-		FileInputStream inputStream =null;
+	/**
+	 * @Title: readTextFile   
+	 * @Description: 以流的方式，读取文本文件内容   
+	 * @param: @param file
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
+	public static String readTextFile(File file) {
+		InputStream inputStream = null;
 		try {
-			 inputStream=new FileInputStream(file);
-			byte[] b=new byte[1024];
-			String str="";
-			while(inputStream.read(b)!=-1) {
-				str+=new String(b);
+			inputStream = new FileInputStream(file);
+			byte[] b = new byte[1024];
+			String str = null;
+			while (inputStream.read(b)!=-1) {
+				str += new String(b);
 			}
 			return str;
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}finally {
 			closeAll(inputStream);
 		}
-		
+	}
+	/**
+	 * @Title: getFileContent   
+	 * @Description: 根据文件全名读取文件内容   
+	 * @param: @param fileFullName
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
+	public static String readTextFile(String fileFullName) {
+		return readTextFile(new File(fileFullName));
 	}
 	
-	
-	public static String getFileContent(String fileName) {
-		return getFileContent(new File(fileName));
+	public static void writeTextFile(String content,File file,boolean append) {
+		BufferedWriter writer = null;
+		try {
+			//判断写文件的文件夹是否存在
+			String parent = file.getParent();
+			File parentFile = new File(parent);
+			if(!parentFile.exists()) {
+				parentFile.mkdirs();
+			}
+			//写文件
+			writer = new BufferedWriter(new FileWriter(file,append));
+			writer.write(content);
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			closeAll(writer);
+		}
 	}
 	
+	public static void writeTextFile(String content,String fileFullName,boolean append) {
+		writeTextFile(content,new File(fileFullName), append);
+	}
+
 	public static void main(String[] args) {
-		System.out.println(getFileContent("F:\\GitTest\\maven-demo2\\readme.txt"));
+		String readTextFile = readTextFile("C:\\Users\\Administrator\\Desktop\\pom.xml");
+		writeTextFile(readTextFile, "C:\\Users\\Administrator\\Desktop\\aa\\aa.xml",false);
 	}
 }
